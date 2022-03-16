3c24a5fc4bb07200de0d957c3c7e3598aef5aa7b
Walter Fan (yafan) <yafan@cisco.com>
2022-03-16 22:57:22 +0800

    add test

---
+++ webrtc_cc/test/estimator_test.cpp
@@ -0,0 +1,18 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include "gtest/gtest.h"
+#include "estimator.h"
+
+using namespace std;
+using namespace testing;
+
+
+
+TEST(EstimatorTest, ComputeSlope)
+{
+    cout<<"--- EstimatorTest, ComputeSlope --"<<endl;
+
+    std::deque<PacketTiming> packets;
+    
+}
